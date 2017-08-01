6035f8c37f7b0fa67e3991fa197b25983dd1964b
Michael Kapelko <kornerr@gmail.com>
2017-08-01 17:44:38 +0700

    Add env.call to Chai script

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 
 #include "Environment.h"
+#include "File.h"
 
+// CPP.
 #include "cpp/module.h"
 #include "cpp/proxy.h"
 
+// Chai.
+#include <chaiscript/chaiscript.hpp>
+
 Environment *env = 0;
 
-void runCPPClients(Environment *env)
+void runChai(Environment *env, const char *fileName)
+{
+    // Create Chai engine.
+    chaiscript::ChaiScript chai;
+    // Register environment type.
+    chai.add(chaiscript::fun(&Environment::addClient), "addClient");
+    chai.add(chaiscript::fun(&Environment::call), "call");
+    chai.add(chaiscript::user_type<Environment>(), "Environment");
+    // Register environment instance.
+    chai.add(chaiscript::var(env), "env");
+    // Load script.
+    String script = readFile(fileName);
+    // Execute script.
+    chai.eval(script);
+}
+
+void runCPP(Environment *env)
 {
     Module *module = new Module;
     env->addClient(module);
@@ -24,17 +45,26 @@ void runCPPClients(Environment *env)
         printf("proxy. values: '%s'\n", stringsToString(values).c_str());
     }
 
+    /*
     // Deallocate 'model' and 'proxy'.
     delete module;
     delete proxy;
+    */
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("%s /path/to/script.chai\n", argv[0]);
+        return 1;
+    }
+    const char *fileName = argv[1];
+
     Environment env;
 
-    runCPPClients(&env);
-
+    runCPP(&env);
+    runChai(&env, fileName);
 
     return 0;
 }
