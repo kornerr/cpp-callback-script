b8bfc47b5565e58d74696795a5316a940c076043
Michael Kapelko <kornerr@gmail.com>
2017-08-02 10:37:11 +0700

    Add stub EnvironmentClient implementation

--- app/EnvironmentClient.h
+++ app/EnvironmentClient.h
@@ -4,13 +4,28 @@
 
 #include "Common.h"
 
+#include <cstdio>
+
 class EnvironmentClient
 {
     public:
+        EnvironmentClient() { }
         virtual ~EnvironmentClient() { }
 
-        virtual bool respondsToKey(const String &key) = 0;
-        virtual Strings call(const String &key, const Strings &values) = 0;
+        virtual bool respondsToKey(const String &key)
+        {
+            printf("EnvironmentClient.respondsToKey(%s). override me!\n", key.c_str());
+            return false;
+        }
+        virtual Strings call(const String &key, const Strings &values)
+        {
+            printf(
+                "EnvironmentClient.call(%s, %s). override me!\n",
+                key.c_str(),
+                stringsToString(values).c_str());
+            Strings stub;
+            return stub;
+        }
 };
 
 #endif // CPP_CALLBACK_SCRIPT_ENVIRONMENT_CLIENT_H
